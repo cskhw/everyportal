ad912f3a026ee902826f88cb818c79b6c4e2d17a
khw0828 <gusdn0828@gmail.com>
2021-03-19 18:31:06 +0900

    랭킹 목록 리팩토링
    을 했읍니다.

--- app/src/main/java/com/swengineering/everyportal/setting/ListSettingActivity.kt
+++ app/src/main/java/com/swengineering/everyportal/setting/ListSettingActivity.kt
@@ -1,6 +1,9 @@
 package com.swengineering.everyportal.setting
 
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.swengineering.everyportal.MainActivity.Companion.mainViewModel
@@ -9,34 +12,71 @@ import com.swengineering.everyportal.utils.AppPreferenceManager
 import kotlinx.android.synthetic.main.activity_list_setting.*
 
 class ListSettingActivity : AppCompatActivity() {
-    var checks: String = ""
+    private var checks: String = ""
+    private val preference = AppPreferenceManager
+    private val tabsString = arrayListOf("네이버", "다음", "줌", "구글")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_setting)
-        val preference = AppPreferenceManager
-        val naver = activity_list_setting_checkbox_naver
-        val daum = activity_list_setting_checkbox_daum
-        val zum = activity_list_setting_checkbox_zum
-        val google = activity_list_setting_checkbox_google
+        val tabsList = arrayListOf<CheckBox>(
+            activity_list_setting_checkbox_naver,
+            activity_list_setting_checkbox_daum,
+            activity_list_setting_checkbox_zum,
+            activity_list_setting_checkbox_google
+        )
+        val linearList = arrayListOf<LinearLayout>(
+            activity_list_setting_linear_naver,
+            activity_list_setting_linear_daum,
+            activity_list_setting_linear_zum,
+            activity_list_setting_linear_google
+        )
+
+        // 탭 리스트 설정
+        setRankingList(tabsList)
+        // 리니어 레이아웃에 리스터 추가
         activity_list_setting_button_back.setOnClickListener { finish() }
-        activity_list_setting_linear_naver.setOnClickListener { naver.isChecked = !naver.isChecked }
-        activity_list_setting_linear_daum.setOnClickListener { daum.isChecked = !daum.isChecked }
-        activity_list_setting_linear_zum.setOnClickListener { zum.isChecked = !zum.isChecked }
-        activity_list_setting_linear_google.setOnClickListener {
-            google.isChecked = !google.isChecked
+        (0..3).forEach { i ->
+            linearList[i].setOnClickListener {
+                tabsList[i].isChecked = !tabsList[i].isChecked
+            }
         }
-        activity_list_setting_button_finish.setOnClickListener {
-            println("설정 완료 버튼 클릭")
-            if (activity_list_setting_checkbox_naver.isChecked) checks += "naver,"
-            if (activity_list_setting_checkbox_daum.isChecked) checks += "daum,"
-            if (activity_list_setting_checkbox_zum.isChecked) checks += "zum,"
-            if (activity_list_setting_checkbox_google.isChecked) checks += "google,"
-            checks = checks.substring(0, checks.length - 1)
-            mainViewModel.checks.value = checks
-            println("check: $checks")
-            preference.setString(this, "rankingList", checks)
-            Toast.makeText(this, "목록이 설정되었습니다.", Toast.LENGTH_LONG).show()
-            finish()
+        activity_list_setting_button_finish.setOnClickListener(onClickFinishButton(tabsList))
+    }
+
+    private fun onClickFinishButton(
+        tabsList: ArrayList<CheckBox>
+    ): View.OnClickListener = View.OnClickListener { v ->
+        if (tabsList[0].isChecked) checks += "네이버,"
+        if (tabsList[1].isChecked) checks += "다음,"
+        if (tabsList[2].isChecked) checks += "줌,"
+        if (tabsList[3].isChecked) checks += "구글,"
+        checks = checks.substring(0, checks.length - 1)
+        mainViewModel.checks.value = checks
+        preference.setString(v.context, "rankingList", checks)
+        Toast.makeText(v.context, "목록이 설정되었습니다.", Toast.LENGTH_LONG).show()
+        finish()
+    }
+
+    private fun setRankingList(tabsList: ArrayList<CheckBox>) {
+        val tabList: List<String> =
+            preference.getString(this, "rankingList")?.split(',') ?: listOf()
+        try {
+            var i = 0
+            tabsString.forEach { a ->
+                tabList.forEach { b ->
+                    if (a == b)
+                        when (i) {
+                            0 -> tabsList[0].isChecked = true
+                            1 -> tabsList[1].isChecked = true
+                            2 -> tabsList[2].isChecked = true
+                            3 -> tabsList[3].isChecked = true
+                        }
+                }
+                i++
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
